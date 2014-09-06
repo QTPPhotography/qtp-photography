72c543be54ddc169e0f7a405884bb74cdcf17139
jtran07 <j.tran07.jt@gmail.com>
2014-09-06 19:41:19 -0400

    Create home.php

---
+++ home.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * @site    QTPPhotography
+ * @page    Home Page
+ * @author  @Jtran07 & @PaperSpeaker
+ * @date    Sepetember 6th, 2014
+ * @version Version 1.0.0
+ */
+
+?>
+
+<html>
+    <head>
+        <title>QTPPhotography</title>
+    </head>
+    
+    <body>
+        <?php echo '<p> QTPPhotography </p>'; ?>
+    </body>
+</html>
