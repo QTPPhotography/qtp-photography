2bddebe2d29148a11a428775984004c8c939ba30
jtran07 <j.tran07.jt@gmail.com>
2014-09-06 19:44:23 -0400

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * @site    QTPPhotography
+ * @page    Contact Page
+ * @author  @Jtran07 & @PaperSpeaker
+ * @date    Sepetember 6th, 2014
+ * @version Version 1.0.0
+ */
+
+?>
+
+<html>
+    <head>
+        <title>Contact Us</title>
+    </head>
+    
+    <body>
+        <?php echo '<p> Contact QTPPhotography </p>'; ?>
+    </body>
+</html>
