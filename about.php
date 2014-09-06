5d083495e64c447a3297b9f95a2075a31e0e5714
jtran07 <j.tran07.jt@gmail.com>
2014-09-06 19:34:20 -0400

    Create about.php

---
+++ about.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * @site    QTPPhotography
+ * @page    About Page
+ * @author  @Jtran07 & PaperSpeaker
+ * @date    Sepetember 6th, 2014
+ * @version Version 1.0.0
+ */
+
+?>
+
+<html>
+    <head>
+        <title>About Page</title>
+    </head>
+    
+    <body>
+        <?php echo '<p> About QTPPhotography </p>'; ?>
+    </body>
+</html>
