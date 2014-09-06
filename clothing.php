de4ffd804563871cb5f84db289735a064b530460
jtran07 <j.tran07.jt@gmail.com>
2014-09-06 19:38:17 -0400

    Create clothing.php

---
+++ clothing.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * @site    QTPPhotography
+ * @page    Weddings Page
+ * @author  @Jtran07 & @PaperSpeaker
+ * @date    Sepetember 6th, 2014
+ * @version Version 1.0.0
+ */
+
+?>
+
+<html>
+    <head>
+        <title>Clothing Page</title>
+    </head>
+    
+    <body>
+        <?php echo '<p> Clothing shoots taken by QTPPhotography </p>'; ?>
+    </body>
+</html>
