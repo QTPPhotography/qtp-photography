a0c7904f155ff5dcf01a348d79389a0f12c917bf
jtran07 <j.tran07.jt@gmail.com>
2014-09-06 19:35:59 -0400

    Create weddings.php

---
+++ weddings.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * @site    QTPPhotography
+ * @page    Weddings Page
+ * @author  @Jtran07 & @PaperSpeaker
+ * @date    Sepetember 6th, 2014
+ * @version Version 1.0.0
+ */
+
+?>
+
+<html>
+    <head>
+        <title>Weddings Page</title>
+    </head>
+    
+    <body>
+        <?php echo '<p> Weddings taken by QTPPhotography </p>'; ?>
+    </body>
+</html>
