074650e1f811b36d4283bb63aededb0d0700c3b1
jtran07 <j.tran07.jt@gmail.com>
2014-09-06 19:55:19 -0400

    Update contact.php
    Added form with validation

--- contact.php
+++ contact.php
@@ -10,12 +10,85 @@
 
 ?>
 
+<!DOCTYPE HTML> 
 <html>
-    <head>
-        <title>Contact Us</title>
-    </head>
-    
-    <body>
-        <?php echo '<p> Contact QTPPhotography </p>'; ?>
-    </body>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body> 
+
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $subjectErr = "";
+$name = $email = $subject = $comment = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   if (empty($_POST["name"])) {
+     $nameErr = "Name is required";
+   } else {
+     $name = test_input($_POST["name"]);
+   }
+   
+   if (empty($_POST["email"])) {
+     $emailErr = "Email is required";
+   } else {
+     $email = test_input($_POST["email"]);
+   }
+   
+      if (empty($_POST["subject"])) {
+     $subjectErr = "Subject is required";
+   } else {
+     $subject = test_input($_POST["subject"]);
+   }
+
+   if (empty($_POST["comment"])) {
+     $comment = "";
+   } else {
+     $comment = test_input($_POST["comment"]);
+   }
+
+
+}
+
+function test_input($data) {
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return $data;
+}
+?>
+
+<h2>PHP Form Validation Example</h2>
+<p><span class="error">* required field.</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+   Name: <input type="text" name="name">
+   <span class="error">* <?php echo $nameErr;?></span>
+   <br><br>
+   E-mail: <input type="text" name="email">
+   <span class="error">* <?php echo $emailErr;?></span>
+   <br><br>
+   Subject: <input type="text" name="subject">
+   <span class="error">* <?php echo $subjectErr;?></span>
+   <br><br>
+   Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+   <br><br>
+   <br><br>
+   <input type="submit" name="submit" value="Submit"> 
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $subject;
+echo "<br>";
+echo $comment;
+echo "<br>";
+?>
+
+</body>
 </html>
