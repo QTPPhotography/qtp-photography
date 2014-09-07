1b044138cde4f3d24b66d85f4fd566b166e3dff9
jtran07 <j.tran07.jt@gmail.com>
2014-09-06 21:29:25 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -60,7 +60,7 @@ function test_input($data) {
 }
 ?>
 
-<h2>PHP Form Validation Example</h2>
+<h2>Contact Form</h2>
 <p><span class="error">* required field.</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
    Name: <input type="text" name="name">
