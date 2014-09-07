0b38db74b5d6785bd2fdc3498f4c938355602a44
jtran07 <j.tran07.jt@gmail.com>
2014-09-06 22:29:45 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -9,15 +9,32 @@
  */
 
 ?>
-
 <!DOCTYPE HTML> 
 <html>
-<head>
-<style>
-.error {color: #FF0000;}
-</style>
-</head>
-<body> 
+    <head>
+        <title>QTPPhotography</title> <!-- title of the current page -->
+        .error {color: #FF0000;}
+    </head>
+    
+    <body>
+        <div id="container"> <!-- open container div -->
+            <div id="header"> <!-- open header div -->
+                <h1>Welcome to QTPPhotography</h1> <!-- h1 tag -->
+            </div> <!-- end header div -->
+            <div id="menu"> <!-- menu div -->
+                <a href="about.php">About</a>
+                <a href="clothing.php">Clothing</a>
+                <a href="nature.php">Nature</a>
+                <a href="engagement.php">Engagements</a>
+                <a href="weddings.php">Weddings</a>
+                <a href="contact.php">Contact</a>
+            </div> <!-- end menu div -->
+            <?php echo '<p> QTPPhotography </p>'; ?>
+            
+            <div id="footer">Copyright&copy; QTPPhotography</div> <!-- footer -->
+        </div> <!-- end container div -->
+    </body>
+</html>
 
 <?php
 // define variables and set to empty values
