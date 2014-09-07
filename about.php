bd1e03d09e72b94c41bb495cd891b529937106f6
jtran07 <j.tran07.jt@gmail.com>
2014-09-06 22:26:54 -0400

    Update about.php

--- about.php
+++ about.php
@@ -5,17 +5,32 @@
  * @page    About Page
  * @author  @Jtran07 & @PaperSpeaker
  * @date    Sepetember 6th, 2014
- * @version Version 1.0.0
+ * @version Version 1.0.1
  */
 
 ?>
 
 <html>
     <head>
-        <title>About Page</title>
+        <title>About QTPPhotography</title> <!-- title of the current page -->
     </head>
     
     <body>
-        <?php echo '<p> About QTPPhotography </p>'; ?>
+        <div id="container"> <!-- open container div -->
+            <div id="header"> <!-- open header div -->
+                <h1>About QTPPhotography</h1> <!-- h1 tag -->
+            </div> <!-- end header div -->
+            <div id="menu"> <!-- menu div -->
+                <a href="about.php">About</a>
+                <a href="clothing.php">Clothing</a>
+                <a href="nature.php">Nature</a>
+                <a href="engagement.php">Engagements</a>
+                <a href="weddings.php">Weddings</a>
+                <a href="contact.php">Contact</a>
+            </div> <!-- end menu div -->
+            <?php echo '<p> QTPPhotography </p>'; ?>
+            
+            <div id="footer">Copyright&copy; QTPPhotography</div> <!-- footer -->
+        </div> <!-- end container div -->
     </body>
 </html>
